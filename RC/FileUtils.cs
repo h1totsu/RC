2969212baca4eec8eed0ec270579efc9f9d1c3df
h1totsu <h1totsu@mail.ru>
2016-05-20 20:54:56 +0300

    Init

---
+++ RC/FileUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RC
+{
+    class FileUtils
+    {
+        public static void ListDirectory(TreeNode node, string path)
+        {
+            node.Nodes.Clear();
+            DirectoryInfo rootDirectoryInfo = new DirectoryInfo(path);
+            CreateDirectoryNode(rootDirectoryInfo, node);
+        }
+
+        private static void CreateDirectoryNode(DirectoryInfo directoryInfo, TreeNode directoryNode)
+        {
+            foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(new TreeNode(directory.Name) { Tag = directory.FullName });
+            foreach (FileInfo file in directoryInfo.GetFiles())
+                directoryNode.Nodes.Add(new TreeNode(file.Name) { Tag = file.FullName });
+        }
+    }
+}
---
+++ RC/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SocketServer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SocketClient.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(new TreeNode("root"));
+            FileUtils.ListDirectory(treeView1.TopNode, @"D:\Arts");
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            treeView1.SelectedNode.Nodes.Clear();
+            FileUtils.ListDirectory(treeView1.SelectedNode, treeView1.SelectedNode.Tag.ToString());
+        }
+    }
+}
